3bba60508e501f473f5a10e19ad08fc1eee670e5
Paige Morton <paigeashley624@gmail.com>
2021-03-16 11:44:13 -0400

    adds validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,9 @@
 class Meeting < ApplicationRecord
   has_many :meeting_speakers
   has_many :speakers, through: :meeting_speakers
+
+  validate :title, presence: true
+  validate :agenda, presence: true
+  validate :location, presence: true
+  validate :time, time: time.now #need to sort out if this is correct
 end
